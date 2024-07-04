90b6aeebae3cfa19bd989849c1b4c7a292105104
ViniciusPitameia <viniciuspitameia1@gmail.com>
2024-07-04 10:48:40 -0300

    .

---
+++ JS/aula3/ex/index.js
@@ -0,0 +1,3 @@
+var nome = 'Vinicius Pitameia'
+var hora = 10
+console.log(`Meu nome é ${nome}. Estou aprendendo JavaScript às ${hora} da manhã.`)
---
+++ JS/aula4/comentarios.js
@@ -0,0 +1,9 @@
+// Comentario em linha
+console.log("Olá, mundo!")
+/*
+Comentario em bloco
+Linha 1
+Linha 2
+*/
+
+console.log("Bora aprender!")
