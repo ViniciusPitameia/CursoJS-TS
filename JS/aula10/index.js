66324de94e1657552aa60c608fdf9bd7aa8f8bbc
ViniciusPitameia <viniciuspitameia1@gmail.com>
2024-07-05 10:19:47 -0300

    Aula 10 Operadores aritméticos

---
+++ JS/aula10/index.js
@@ -0,0 +1,14 @@
+/*      ----- Aritméticos -----
+* ---- Adição / Concatenação (+) ----
+* ---- Subtração (-), Divisão (/), Multiplicação (*) e Potenciação (**) ----
+* ---- Resto da divisão (%) ----
+       ----- Precêdencia -----
+* ---- () ----
+* ---- ** ----
+* ---- / * % ----
+* ---- + - ----
+*/
+
+const num1 = 10
+const num2 = 20
+console.log(num1 * num2)
