8d11207d9e1cd28c482f46467d026cbcfb3d20a5
ViniciusPitameia <viniciuspitameia1@gmail.com>
2024-07-05 10:53:02 -0300

    Aula 11 Exercício de adição com prompt

---
+++ JS/aula11/js/index.js
@@ -0,0 +1,12 @@
+/* alert('Com a nossa mensagem.')
+
+confirm('Você deseja apagar esse item?')
+
+prompt('Qual é seu nome?') */
+
+let num1 = prompt('Digite um número:')
+let num2 = prompt('Digite outro número:')
+num1 = Number(num1)
+num2 = Number(num2)
+let resultado = num1 + num2
+alert(`O resultado da sua soma é ${resultado}`)
