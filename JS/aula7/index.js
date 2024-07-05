633fc855cc2e2303cec9ce768bc33ea41d99f4d8
ViniciusPitameia <viniciuspitameia1@gmail.com>
2024-07-05 02:54:30 -0300

    Aula 7 constante

---
+++ JS/aula7/index.js
@@ -0,0 +1,10 @@
+const nome = 'João'
+
+console.log(nome)
+
+const primeiroNumero = 5 
+const segundoNumero = 10
+const resultado = primeiroNumero * segundoNumero
+const resultadoDuplicado = resultado * 2
+console.log(`${resultado}`)
+console.log(`${resultadoDuplicado}`)
