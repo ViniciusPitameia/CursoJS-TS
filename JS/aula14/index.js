306a3b307a9785c48cc6562533ac3cb7e479b435
ViniciusPitameia <viniciuspitameia1@gmail.com>
2024-07-07 03:41:36 -0300

    Aula 14 Number

---
+++ JS/aula14/index.js
@@ -0,0 +1,8 @@
+let num1 = 0.7;
+let num2 = 0.1;
+
+
+console.log(num1 + num2)
+console.log(num1.toFixed(2)) // Escolhe quantas casas decamis mostrar.
+console.log(Number.isInteger(num1)) // Diz se numero da variavel é inteiro
+// num1 = parseFloat(num1.toFixed(2))  Resolve problema de numeros float
