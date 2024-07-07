cf0f58948f644b76800d30d6f0c5fb3916c434b7
ViniciusPitameia <viniciuspitameia1@gmail.com>
2024-07-07 03:59:58 -0300

    Aula 15 Objeto Math

---
+++ JS/aula15/index.js
@@ -0,0 +1,12 @@
+
+
+// let num2 = Math.floor(num1) Arredonda pra baixo ex: 9.6 == 9
+// let num2 = Math.ceil(num1) Arredonda pra cima ex 9.6 == 10
+// let num2 = Math.round(num1) Arredonda pro numero inteiro mais proximo ex: 9.49 == 9 / 9.50 == 10
+// console.log(Math.max(1,2,3,4,5,15,21,5848,1541,215,-458)) Seleciona o maior numero de uma lista
+// console.log(Math.min(1,2,3,4,5,15,21,5848,1541,215,-458)) Seleciona o menor numero de uma lista
+// console.log(Math.random()) Gera um numero aleatorio entra 0 e 1 (1 nunca é gerado) (sempre um numero fracionado)
+
+/*const aleatorio = Math.round(Math.random() * (21 - 1) + 1) Ideia para dado de RPG
+console.log(aleatorio) */
+
