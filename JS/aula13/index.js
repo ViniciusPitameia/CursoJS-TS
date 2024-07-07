b88cfff198bc0b2f31573b3c4e023f98ea927da5
ViniciusPitameia <viniciuspitameia1@gmail.com>
2024-07-07 07:38:21 -0300

    Aula 13 Exercício de string no navegador

---
+++ JS/aula13/index.js
@@ -0,0 +1,15 @@
+const nome = 'Vinícius Pitaméia Falcão'
+
+console.log(nome.length) // quantia de caracteres na variavel
+
+console.log(nome.split(' ')) // sepera as palavras pelo espaço
+
+console.log(nome.indexOf('i')) // seleciona o primeiro indice da letra 'i'
+
+console.log(nome.lastIndexOf('i')) // seleciona o último indice da letra 'i'
+
+console.log(nome.toUpperCase()) // Todas letras maiúsculas
+
+console.log(nome.toLowerCase()) // Todas letras minúscula
+
+console.log(nome.slice(-5)) // corta a variavel
