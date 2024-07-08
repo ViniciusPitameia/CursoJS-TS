294866bdf959009f7383658cbd583638d53d3ce9
ViniciusPitameia <viniciuspitameia1@gmail.com>
2024-07-08 13:28:32 -0300

    Aula 17 Introdução básica: Funções em JS

---
+++ JS/aula17/index.js
@@ -0,0 +1,18 @@
+/*function saudacao(nome) {
+    return `Bom dia ${nome}!`;
+}
+const variavel = saudacao('Vinicius');
+console.log(variavel);*/
+/*function soma(x = 0, y = 0) {  // valores pre-definidos 
+    const resultado = x + y;
+    return resultado;
+} */
+// console.log(soma(2, 2));
+// const resultado = soma(8, 9);  // substitiu os numeros pre-definidos
+//console.log(resultado);
+
+const raiz = n => n ** 0.5;  // Mesma coisa que a funcion
+
+console.log(raiz(9));
+console.log(raiz(64));
+console.log(raiz(16));
