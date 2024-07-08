1ef1d4331ade42268813fdb1189bf2383a3798c6
ViniciusPitameia <viniciuspitameia1@gmail.com>
2024-07-08 12:52:24 -0300

    Aula 16 Array (basico)

---
+++ JS/aula16/index.js
@@ -0,0 +1,14 @@
+const alunos = ['Otávio', 'Luiz', 'Ricardo'];
+
+// alunos[0] = 'Salem'   / Irá mudar indice indicado para o que vc quiser
+// alunos[3] = 'Aurora'  / Adiciona mais um indice no array (Se souber quantos indice ja tem)
+// console.log(alunos.length)  / Ver tamanho da array
+// alunos[alunos.length] = 'Luiza';  // Adiciona o proximo elemento
+// alunos.push('Vinicius'); // Adiciona no fim do array
+// alunos.unshift('Fábio'); // Adiciona no começo do array
+// alunos.pop(); // Remove ultimo elemento do array
+// alunos.shift(); // Remove primeiro elemento do array
+// delete alunos[1]; // Delete um indice sem mexer nos outros
+
+console.log(alunos);
+// console.log(alunos.slice(0, -2))  // Corta elementos desejado
