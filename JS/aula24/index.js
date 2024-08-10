cf5b395324047c70fa980e8afa3adcdfdce12d7c
ViniciusPitameia <167634087+ViniciusPitameia@users.noreply.github.com>
2024-08-10 01:40:13 -0300

    Aula 24 Basico de if e else Parte1

---
+++ JS/aula24/index.js
@@ -0,0 +1,29 @@
+// Eu posso ter vários else ifs na checagem
+// If pode ser usado sozinho
+// Sempre que utilizo a palavra else, preciso de um if antes
+// Podemos usar condições sem else if, utilizando apenas if e else
+
+/*
+const hora = 5;
+
+if (hora <= 4) {
+    console.log('Boa noite!');
+} else if (hora >= 5 && hora < 12) {
+    console.log('Bom dia!');
+} else if (hora >= 13 && hora <= 18) {
+    console.log('Boa tarde!');
+} else if (hora <= 4 && hora > 18) {
+    console.log('Boa noite!');
+} else {
+    console.log('Olá!');
+}
+*/
+
+const tenhoGrana = true;
+// const tenhoGrana = false;
+
+if (tenhoGrana) {
+    console.log('Vou sair de casa.');
+} else {
+    console.log('Não vou sair de casa.');
+}
