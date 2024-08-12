aae6262b457bdd285e77f71ba72f86f5541d0959
ViniciusPitameia <167634087+ViniciusPitameia@users.noreply.github.com>
2024-08-12 04:20:55 -0300

    Aula 28  Objeto Date

---
+++ JS/aula28/index.js
@@ -0,0 +1,31 @@
+/*
+const data = new Date(); // 01/01/1970 Timestamp unix ou época unix
+console.log('Dia', data.getDate());
+console.log('Mês', data.getMonth() + 1); // Mês começa do zero
+console.log('Ano', data.getFullYear());
+console.log('Hora', data.getHours());
+console.log('Min', data.getMinutes());
+console.log('Seg', data.getSeconds());
+console.log('Dia da semana', data.getDay());  // 0 = Domingo, 6 = Sabado
+
+console.log(data.toString());
+*/
+
+function zeroAEsquerda (num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+function formataData(data) {
+    const dia = zeroAEsquerda (data.getDate());
+    const mes = zeroAEsquerda (data.getMonth() + 1);
+    const ano = zeroAEsquerda (data.getFullYear());
+    const hora = zeroAEsquerda (data.getHours());
+    const min = zeroAEsquerda (data.getMinutes());
+    const seg = zeroAEsquerda (data.getSeconds());
+
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg} `;
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
