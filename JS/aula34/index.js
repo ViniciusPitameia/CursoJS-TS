2da4cee101ac78aedbdfe1e9d89e92d8e1ab4923
ViniciusPitameia <167634087+ViniciusPitameia@users.noreply.github.com>
2024-08-13 01:33:40 -0300

    Aula 34 For (clássico) Estrutura de repetição

---
+++ JS/aula34/index.js
@@ -0,0 +1,14 @@
+// console.log('Linha 0');
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+// console.log('Linha 6');
+
+const frutas = ['Maça', 'Pêra', 'Uva', 'Melancia', 'Morango', 'Limão'];
+
+for (let i = 0; i < frutas.length; i++) {
+    //const par = i % 2 === 0 ? 'par' : 'ímpar';
+    console.log(frutas[i]);
+}
