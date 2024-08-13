e8eed6325e04e6aa99a3d9f6b510d50d175c13ca
ViniciusPitameia <167634087+ViniciusPitameia@users.noreply.github.com>
2024-08-13 02:02:56 -0300

    Aula 35 Exercícios com For

---
+++ JS/aula35/assets/js/main.js
@@ -0,0 +1,18 @@
+const elementos = [
+    {tag: 'p', texto: 'Frase 1'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'section', texto: 'Frase 3'},
+    {tag: 'footer', texto: 'Frase 4'},
+];
+
+const conteiner = document.querySelector('.conteiner');
+const div = document.createElement('div');
+
+for (let i = 0; i < elementos.length; i++) {
+    let { tag, texto } = elementos[i];
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerText = texto;
+    div.appendChild(tagCriada);
+}
+
+conteiner.appendChild(div);
